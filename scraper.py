b6a149a09b6b6ae0e7183b0e3ea9422dff575e4b
Ashish Saini <sainiashish08@gmail.com>
2018-01-01 20:59:30 +0530

    Fix for morph.io python3 compability

--- scraper.py
+++ scraper.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+# morph.io requires this db filename, but scraperwiki doesn't nicely
+# expose a way to alter this. So we'll fiddle our environment ourselves
+# before our pipeline modules load.
+os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 import re
 import string
 import scrapy
